ebd18a7a5f5d4d33946bc73e3acd911d3cee180d
Federico Parola <federico.parola@polito.it>
2023-06-21 21:48:42 +0000

    Add Unikraft latency test

---
+++ apps/rr-latency-unikraft/qemu-ifup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TAP interface will be passed in $1
+bridge=br0
+guest_device=$1
+ip link set $guest_device up
+brctl addif $bridge $guest_device
---
+++ apps/rr-latency-unikraft/run_client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+eval qemu-system-x86_64 \
+	-nographic \
+	-vga none \
+	-net none \
+	-kernel ./build/rr-latency-unikraft_qemu-x86_64 \
+	-enable-kvm \
+	-cpu host \
+	-netdev tap,id=tap1,script=qemu-ifup.sh,downscript=no,vhost=on \
+        -device virtio-net-pci,netdev=tap1,mac=52:54:00:12:34:57 \
+        -append \"netdev.ipv4_addr=10.0.0.2 netdev.ipv4_gw_addr=10.0.0.254 netdev.ipv4_subnet_mask=255.255.255.0 -- -c "$@"\"

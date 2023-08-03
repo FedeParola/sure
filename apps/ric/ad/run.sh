1ad88b4f4743094ad16946ae3f1d4d1daa76b339
Federico Parola <federico.parola@polito.it>
2023-08-03 20:59:02 +0000

    ric: Use hugepages for shm

--- apps/ric/ad/run.sh
+++ apps/ric/ad/run.sh
@@ -7,8 +7,9 @@ eval $HOME/src/qemu/build/qemu-system-x86_64 \
 	-kernel "$(dirname $0)/build/ad_qemu-x86_64" \
 	-enable-kvm \
 	-cpu host \
+	-m 512 \
 	-device ivshmem-doorbell,vectors=1,chardev=id \
 	-chardev socket,path=/tmp/ivshmem_socket,id=id \
-	-object memory-backend-file,size=1M,share=true,mem-path=/dev/shm/unimsg_buffers,id=hostmem \
+	-object memory-backend-file,size=2M,share=true,mem-path=/dev/hugepages/unimsg_buffers,id=hostmem \
 	-device ivshmem-plain,memdev=hostmem \
         -append \""$@"\"

bf53c963d419a91010da3638433c244ed5ff40ee
Federico Parola <federico.parola@polito.it>
2023-11-29 13:30:19 +0000

    throughput: Fix unikraft scripts

--- apps/throughput/unikraft/run_vm.sh
+++ apps/throughput/unikraft/run_vm.sh
@@ -16,5 +16,5 @@ eval qemu-system-x86_64 \
 	-enable-kvm \
 	-cpu host \
 	-netdev tap,id=tap0,script=$(dirname $0)/qemu-ifup.sh,downscript=no,vhost=on \
-        -device virtio-net-pci,netdev=tap0,mac=52:54:00:12:34:5$1 \
-        -append \"netdev.ipv4_addr=10.0.0.$1 netdev.ipv4_gw_addr=10.0.0.254 netdev.ipv4_subnet_mask=255.255.255.0 -- "$@"\"
+        -device virtio-net-pci,netdev=tap0,mac=52:54:00:12:34:5$id \
+        -append \"netdev.ipv4_addr=10.0.0.$id netdev.ipv4_gw_addr=10.0.0.254 netdev.ipv4_subnet_mask=255.255.255.0 -- "$@"\"

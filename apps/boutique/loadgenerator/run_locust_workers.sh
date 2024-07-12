ce5af46326627d4076053c5a75663a73aeaf5d14
Federico Parola <federico.parola@polito.it>
2024-07-12 10:12:14 -0500

    boutique/loadgenerator: Improve tests script

--- apps/boutique/loadgenerator/run_locust_workers.sh
+++ apps/boutique/loadgenerator/run_locust_workers.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-workers=$1
-shift
+if [ $# -ne 3 ]; then
+        echo "usage: $0 <url> <time> <clients>"
+	exit 1
+fi
+
+url=$1
+time=$2
+clients=$3
+ncpus=$(nproc)
+if [ $ncpus -lt $clients ]; then
+        workers=$ncpus
+else
+        workers=$clients
+fi
+
+echo "Running $clients clients on $workers workers"
 
 locust --worker --processes $workers &
-locust --master $@
+locust --master --headless -H $url --csv res -t $time -u $clients -r $clients
